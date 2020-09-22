736de2829612c7992d33cea63dd4ea4fc5e03419
s4mking <samuel.simonney@golem.ai>
2020-09-22 09:18:23 +0200

    Test to connect database online

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AppFixtures extends Fixture
+{
+    private $encoder;
+
+    /**
+     * Encodeur de mot de passe
+     *
+     * @param UserPasswordEncoderInterface $encoder
+     */
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        // admin user
+        $user = new User();
+        $plainPassword = 'motdepas';
+        $encoded = $this->encoder->encodePassword($user, $plainPassword);
+
+        $user->setUsername('admin')
+            ->setRoles(['ROLE_ADMIN'])
+            ->setPassword($encoded);
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
